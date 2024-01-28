85158ac4dce7be447d0f52ca9fc2379a6e55d264
shaunguimond <shaun.guimond@outlook.com>
2024-01-28 14:22:37 -0800

    Merge Fred branch and main.

--- Scenes/UI/level_ui.gd
+++ Scenes/UI/level_ui.gd
@@ -3,7 +3,6 @@ extends CanvasLayer
 @onready var healthContainer = $Control/HBoxContainer/HealthContainer
 @onready var scoreContainer = $Control/HBoxContainer/ScoreContainer
 @onready var timeContainer = $Control/HBoxContainer/TimeContainer
-@onready var tomatoContainer = $Control/HBoxContainer/TomatoContainer
 # Called when the node enters the scene tree for the first time.
 
 @onready var hearts = healthContainer.get_node("HBoxContainer").get_children()
@@ -14,8 +13,6 @@ extends CanvasLayer
 
 func _ready():
 	pass # Replace with function body.
-	
-
 
 func gain_health():
 	current_heart_index = min(len(hearts)-1, current_heart_index+1)
@@ -24,11 +21,6 @@ func gain_health():
 func lose_health():
 	hearts[current_heart_index].texture = empty_heart_tex
 	current_heart_index = max(0, current_heart_index-1)
-	
-func update_tCounter(t):
-	tomatoContainer.get_node("Label2").text = str(t)
-	
-	#tomatoContainer.get_node("Label2").text = count + 1
 
 func update_score(new_score):
 	scoreContainer.get_node("Label2").text = str(new_score)
